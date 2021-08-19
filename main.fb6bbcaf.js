27d211674d9e109703f0c587c0ab1df4c6b37352
Eliezer516 <ybarretomaterano@gmail.com>
2021-08-19 07:43:50 -0400

    colores agregados

--- main.fb6bbcaf.js
+++ main.fb6bbcaf.js
@@ -118,7 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/main.js":[function(require,module,exports) {
-alert('msg');
+
 },{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
